9257e865d9b1c0a46541f1f40f3f49ca1a6147fe
LorenaCornut <152873988+LorenaCornut@users.noreply.github.com>
2025-05-21 18:30:58 +0300

    laborator11-compulsory

---
+++ laborator11-compulsory/src/main/java/org/example/entity/Country.java
@@ -0,0 +1,64 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "countries")
+public class Country {
+
+    @Id
+    @SequenceGenerator(name = "country_seq", sequenceName = "country_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "country_seq")
+
+    private Long id;
+
+    private String name;
+
+    private String code;
+
+    @ManyToOne
+    @JoinColumn(name = "continent_id", referencedColumnName = "id")
+    private Continent continent;
+
+    public Country() {}
+
+    public Country(Long id, String name, String code, Continent continent) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+        this.continent = continent;
+    }
+
+    // Getteri și setteri
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Continent getContinent() {
+        return continent;
+    }
+
+    public void setContinent(Continent continent) {
+        this.continent = continent;
+    }
+}
